cfaaaa46360e256e384e8c99b69438749036e594
ayepei <1624852816@qq.com>
2024-11-28 10:53:44 +0800

    finish exercise6

--- arceos/exercises/simple_hv/src/main.rs
+++ arceos/exercises/simple_hv/src/main.rs
@@ -102,12 +102,25 @@ fn vmexit_handler(ctx: &mut VmCpuRegisters) -> bool {
             }
         },
         Trap::Exception(Exception::IllegalInstruction) => {
+            ax_println!("Bad instruction: {:#x} sepc: {:#x}",
+                stval::read(),
+                ctx.guest_regs.sepc);
+            ctx.guest_regs.gprs.set_reg(A1, 0x1234);
+            
+            ctx.guest_regs.sepc+=4;
+            return false;
             panic!("Bad instruction: {:#x} sepc: {:#x}",
                 stval::read(),
                 ctx.guest_regs.sepc
             );
         },
         Trap::Exception(Exception::LoadGuestPageFault) => {
+            ax_println!("LoadGuestPageFault: stval{:#x} sepc: {:#x}",
+                stval::read(),
+                ctx.guest_regs.sepc);
+            ctx.guest_regs.gprs.set_reg(A0, 0x6688);
+            ctx.guest_regs.sepc+=4;
+            return false;
             panic!("LoadGuestPageFault: stval{:#x} sepc: {:#x}",
                 stval::read(),
                 ctx.guest_regs.sepc
